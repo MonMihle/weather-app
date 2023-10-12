8b5875b73bea3a44bfc11521b1b9d8aceb854ccb
MonMihle <145695872+MonMihle@users.noreply.github.com>
2023-10-12 16:10:08 +0200

    Display forecast data using the API

--- src/app.js
+++ src/app.js
@@ -32,22 +32,34 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  days.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forecast-date">$forecastDay.dt}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
+                ${index}
                 <img
-                  src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""    
                   width="42"
                 />
-                <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">${forecastDay.temp.max}°</span>
-                  <span class="weather-forecast-temperature-min">${forecastDay.temp.min}°</span>
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div> 
+                
             </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
